ecb7b9078d9c1b1308fe17036ac8e18e811e7d0e
Jeremy Tammik <jeremy.tammik@eur.autodesk.com>
2017-02-15 20:38:46 +0100

    incremented version number

--- SetoutPoints/Properties/AssemblyInfo.cs
+++ SetoutPoints/Properties/AssemblyInfo.cs
@@ -34,6 +34,7 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 // 2014-11-03 2015.0.0.1 added instance transformation suggested by sanjaymann
 // 2015-09-09 2016.0.0.0 flat migration to Revit 2016
+// 2017-02-15 2017.0.0.0 flat migration to revit 2017 added test file upgraded setoutpoint symbol family
 //
-[assembly: AssemblyVersion( "2016.0.0.0" )]
-[assembly: AssemblyFileVersion( "2016.0.0.0" )]
+[assembly: AssemblyVersion( "2017.0.0.0" )]
+[assembly: AssemblyFileVersion( "2017.0.0.0" )]
